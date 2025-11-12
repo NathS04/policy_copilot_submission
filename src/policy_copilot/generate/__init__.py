e2e9d92354a0bd6cbea5b0f8a5f4fa5097c0d4c0
Nathaniel Sebastian <sc232ns@leeds.ac.uk>
2025-11-12 06:52:00 +0000

    feat(generate): add prompt templates for naive RAG

---
+++ src/policy_copilot/generate/__init__.py
@@ -0,0 +1,7 @@
+from .answerer import Answerer as Answerer
+from .schema import (
+    RAGResponse as RAGResponse,
+    make_insufficient as make_insufficient,
+    make_llm_disabled as make_llm_disabled,
+)
+from .prompts import format_evidence_block as format_evidence_block
