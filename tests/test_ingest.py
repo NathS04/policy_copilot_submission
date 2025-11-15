79dec22105a7463c4fc3c348c2e40d2a98c89002
Nathaniel Sebastian <sc232ns@leeds.ac.uk>
2025-11-15 03:07:00 +0000

    keep working on it

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -2,3 +2,7 @@ from policy_copilot.ingest.paragraph_ids import generate_paragraph_id
 from policy_copilot.ingest.chunking import clean_paragraph
 
 def test_paragraph_id_determinism():
+    """Test that the same input produces the exact same ID."""
+    doc_id = "test_doc"
+    page = 1
+    idx = 0
