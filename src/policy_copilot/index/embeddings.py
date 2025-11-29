00a98f3edc44efd4d5627230c0cb65781cbcf8fa
Nathaniel Sebastian <sc232ns@leeds.ac.uk>
2025-11-29 04:56:00 +0000

    update embeddings.py

--- src/policy_copilot/index/embeddings.py
+++ src/policy_copilot/index/embeddings.py
@@ -3,3 +3,8 @@ import numpy as np
 from policy_copilot.config import settings
 from policy_copilot.logging_utils import setup_logging
 
+logger = setup_logging()
+
+_model_instance = None
+_sentence_transformer_cls = None
+
